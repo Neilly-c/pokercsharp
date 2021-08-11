905e36abe113df653fede436fb5cf6cd84b46fd5
Neilly-c <46792602+Neilly-c@users.noreply.github.com>
2021-08-11 16:29:22 +0900

    Update FinalHandsDict.cs

--- pokercsharp/pokercsharp/mainsource/appendix/FinalHandsDict.cs
+++ pokercsharp/pokercsharp/mainsource/appendix/FinalHandsDict.cs
@@ -4,6 +4,7 @@ namespace pokercsharp.mainsource.appendix {
 	class FinalHandsDict{
     	const int FULL_DECK_LEN = 52;
 		static Dictionary<Card[], FinalHand> finalDict = new Dictionary<Card[], FinalHand>();
+		static Dictionary<Card[], FinalHand> finalHoldemDict = new Dictionary<Card[], FinalHand>();
 	
 		public void Init(){
             
@@ -12,13 +13,32 @@ namespace pokercsharp.mainsource.appendix {
                 card_arr[i] = new Card(CardValueExt.GetCardValueFromInt(1 + (i / 4)), SuitExt.GetSuitFromInt(1 + (i % 4)));     //カード配列生成
             }
 			
-            int loop = 0;
-            HoldemHandEvaluator evaluator = new HoldemHandEvaluator();  //ハンド評価クラス
-			
 			/*
 			52_C_5 = 2598960 loops.
 			52_C_7 = 133784560 loops.
 			*/
+			
+            int loop = 0;
+            HandEvaluator evaluator = new HandEvaluator();
+			
+			for(int a = 0; a<FULL_DECK_LEN; ++a) {
+				for(int b = a + 1; b<FULL_DECK_LEN; ++b) {
+					for (int c = b + 1; c < FULL_DECK_LEN; ++c) {
+						for (int d = c + 1; d < FULL_DECK_LEN; ++d) {
+							for (int e = d + 1; w < FULL_DECK_LEN; ++e) {
+								Card[] cards = new Card[]{card_arr[a], card_arr[b], card_arr[c], card_arr[d], card_arr[e]};
+								Array.Sort(cards);
+								Array.Reverse(cards);
+								finalDict.Add(cards, evaluator.Evaluate(cards));
+								++loop;
+								if((loop % 25989) - ((loop - 1) % 25989 != 0){
+									Debug.WriteLine(loop % 25989 + "% complete");
+								}
+							}
+						}
+					}
+				}
+			}
 			
 			for(int a = 0; a<FULL_DECK_LEN; ++a) {
 				for(int b = a + 1; b<FULL_DECK_LEN; ++b) {
@@ -31,7 +51,7 @@ namespace pokercsharp.mainsource.appendix {
 																		 , card_arr[e], card_arr[f], card_arr[g]};
 										Array.Sort(hand_n_board);
 										Array.Reverse(hand_n_board);
-										finalDict.Add(hand_n_board, evaluator.Evaluate(hand_n_board));
+										finalHoldemDict.Add(hand_n_board, evaluator.Evaluate(hand_n_board));
 									}
 								}
 							}
